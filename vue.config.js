71e3c165aee52b2b454969e2df70913d0f541f09
sonin <1457065857@qq.com>
2022-04-18 16:42:35 +0800

    开启gzip图片

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,32 @@
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+
 module.exports = {
     lintOnSave: true,
     productionSourceMap: false,
+    configureWebpack: config => {
+        // 开启gzip图片压缩
+        if (CompressionWebpackPlugin) {
+            const productionGzipExtensions = ['html', 'js', 'css'];
+            config.plugins.push(
+                new CompressionWebpackPlugin({
+                    filename: '[path].gz[query]',
+                    algorithm: 'gzip',
+                    test: new RegExp(
+                        '\\.(' + productionGzipExtensions.join('|') + ')$'
+                    ),
+                    threshold: 10240, // 只有大小大于该值的资源会被处理10240
+                    minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+                    deleteOriginalAssets: false // 删除原文件
+                })
+            );
+        }
+    },
     devServer: {
         // 反向代理配置
         proxy: {
             '/boot': {
-                // target: 'http://127.0.0.1:5050',
-                target: 'http://192.168.2.124:8081',
+                // target: 'http://127.0.0.1:8081',
+                target: 'http://192.168.2.124:8080',
                 ws: true
             }
         }
