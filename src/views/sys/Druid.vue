f8e19f2d155994db015ca969fd7a9beba8db673d
sonin <1457065857@qq.com>
2022-04-30 08:47:58 +0800

    Druid监控页面

---
+++ src/views/sys/Druid.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='druid'>
+        <iframe width='1070px' height='566px' :src='iframeSrc'></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Druid',
+        computed: {
+            iframeSrc() {
+                return window.location.origin + '/boot/druid/index.html';
+            }
+        }
+    };
+</script>
+
+<style lang='scss' scoped>
+    .druid {
+        width: 100%;
+        height: 100%;
+    }
+
+</style>
